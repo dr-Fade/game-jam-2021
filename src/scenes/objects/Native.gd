6b5c7592a4e9d228f4c71e6f9f192ab5b7e0f438
dr-Fade <eugenefade@gmail.com>
2021-01-30 18:11:11 +0200

    added 'wounded' effect

--- src/scenes/objects/Native.gd
+++ src/scenes/objects/Native.gd
@@ -2,6 +2,6 @@ extends AbstractObject
 class_name Native
 
 func _ready():
-	set_effect(Traits.Effect.WOODING)
+	set_effect(Traits.Effect.WOUNDING)
 	add_trait(Traits.Trait.EMPTY)
 	add_trait(Traits.Trait.EMPTY)
